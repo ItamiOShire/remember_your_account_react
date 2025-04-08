bdd965f12d7c64de71e48d1fc866d2b79449889a
Adam <a.niewczas01@gmail.com>
2025-04-08 20:35:54 +0200

    Add: navigation bar component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,36 @@
+import logo from '../../assets/images/remgemLogo.svg';
+
+function Navbar() {
+
+    return (
+
+        <div className = "navbar">
+
+            <div className='navbar-logo-container'>
+
+                <img src = {logo} alt = "logo" className = "navbar-logo" />
+
+            </div>
+
+            <nav className = 'navbar-links-container'>
+
+                <a href = "/login" className = "navbar-link">Login</a>
+                <a href = "/register" className = "navbar-link">Register</a>
+                <a href = "/about" className = "navbar-link">About</a>
+                <a href = "/contact" className = "navbar-link">Contact</a>
+
+            </nav>
+
+            <div className = 'navbar-text-container'>
+
+                <p className = "navbar-text">REMGEM</p>
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export default Navbar;
